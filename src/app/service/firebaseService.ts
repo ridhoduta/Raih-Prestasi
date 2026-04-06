90c2c5196a71ca6cefdd40b0ac3dc5a9fe17d267
ridhoduta <ridhodutay@gmail.com>
2026-04-06 08:34:49 +0700

    update FCM

--- src/app/service/firebaseService.ts
+++ src/app/service/firebaseService.ts
@@ -1,6 +1,6 @@
 import admin from "firebase-admin";
 
-const serviceAccount = require("./serviceAccountKey.json");
+const serviceAccount = require("../../lib/serviceAccountKey.json");
 
 if (!admin.apps.length) {
   admin.initializeApp({
